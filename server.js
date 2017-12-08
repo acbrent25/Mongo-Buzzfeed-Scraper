64e32a2a5be59a387e791607a1425a1dc80fc878
Adam Champagne <adamchampagne@gmail.com>
2017-12-08 07:02:11 -0500

    running app

--- server.js
+++ server.js
@@ -27,8 +27,10 @@ app.use(bodyParser.urlencoded({ extended:false }));
 app.use(express.static("public"));
 
 // set mongoose to use ES6 Promis library and Connect to mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/mongoBuzzfeedScraper", {
+mongoose.connect(MONGODB_URI, {
     useMongoClient: true
 });
 
@@ -78,6 +80,41 @@ app.get("/articles", function(req, res) {
     });
   });
 
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get('/articles/:id', function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate('note')
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post('/articles/:id', function(req, res) {
+  // TODO
+  // ====
+  // save the new note that gets posted to the Notes collection
+  // then find an article from the req.params.id
+  // and update it's "note" property with the _id of the new note
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // Take returned note and push it's _id into Articles.note array. Set new: true so it returns updated Article and not old one
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 
 
